6eefa0ae339ce02c3e65a1bc2bb000111b9ccb39
coati-00 <coati.00@gmail.com>
2015-03-19 12:46:19 -0400

    before branch

--- js/respiration_graph.js
+++ js/respiration_graph.js
@@ -23,98 +23,135 @@ jQuery(document).ready(function() {
 	   	
 	});
 	
+	
+	var SpeciesCollectionView = BaseListView.extend({
+		initialize: function (options) {
+			_.bindAll(this, 'renderCollection', 'addItem');
+		    this.collection = new SpeciesCollection(options);
+	        this.collection.on('reset', this.renderCollection);
+	        this.collection.on('add', this.addItem);
+	        this.item_view = SpeciesRowView;
+            console.log("Initialize ScenarioRowView");
+		}//,
+//		
+//	   	events: {
+//	   	    'click .deleteSpecies' : 'deleteSpecies',
+//	   	},
+//	   	
+//	   	deleteScenario: function(e)
+//	    {   
+//	   		console.log("deleteSpecies");
+//	   		
+//	    }
+	   	
+	});
+	
+	
 	var ScenarioRowView = DeletableItemView.extend({
 		initialize: function (options) {
 			//_.bindAll(this, 'renderCollection', 'addItem', 'showPredefinedSpecies');
 		    this.numSpecies = 0;
-		    this.speciesList = new SpeciesCollection();
+		    this.speciesListView = new SpeciesCollectionView();
 		    this.predefinedSpecies = new SpeciesCollection();
 		    this.predefinedSpecies.add([
-		        new Species({   'id': "quercus_rubra",
+		        new Species({   
+		        	'id': "quercus_rubra",
 		            'label' : 'Quercus rubra',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.602,
 		            'e0' : 43140
 		        }),
-		        new Species({   'id': "quercus_prinus",
+		        new Species({   
+		        	'id': "quercus_prinus",
 		            'label' : 'Quercus prinus',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.670,
 		            'e0' : 37005
 		        }),
-		        new Species({   'id': "acer_rubrum",
+		        new Species({   
+		        	'id': "acer_rubrum",
 		            'label' : 'Acer rubrum',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.680,
 		            'e0' : 27210
 		        }),
-		        new Species({   'id': "vaccinium_corymbosum",
+		        new Species({   
+		        	'id': "vaccinium_corymbosum",
 		            'label' : 'Vaccinium corymbosum',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.091,
 		            'e0' : 62967
 		        }),
-		        new Species({   'id': "berberis_thumbergii",
+		        new Species({   
+		        	'id': "berberis_thumbergii",
 		            'label' : 'Berberis thumbergii',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.203,
 		            'e0' : 81950
 		        }),
-		        new Species({   'id': "kalmia_latifolia",
+		        new Species({   
+		        	'id': "kalmia_latifolia",
 		            'label' : 'Kalmia latifolia',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.308,
 		            'e0' : 54940
 		        }),
-		        new Species({   'id': "carya_glabra",
+		        new Species({   
+		        	'id': "carya_glabra",
 		            'label' : 'Carya glabra',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.134,
 		            'e0' : 70547.5
 		        }),
-		        new Species({   'id': "liriodendron_tulipifera",
+		        new Species({   
+		        	'id': "liriodendron_tulipifera",
 		            'label' : 'Liriodendron tulipifera',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.187,
 		            'e0' : 60620.0
 		        }),
-		        new Species({   'id': "platanus_occidentalis",
+		        new Species({   
+		        	'id': "platanus_occidentalis",
 		            'label' : 'Platanus occidentalis',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.320,
 		            'e0' : 56336.7
 		        }),
-		        new Species({   'id': "betula_papyrifera",
+		        new Species({   
+		        	'id': "betula_papyrifera",
 		            'label' : 'Betula papyrifera',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.357,
 		            'e0' : 45322.0
 		        }),
-		        new Species({   'id': "populus_tremuloides",
+		        new Species({   
+		        	'id': "populus_tremuloides",
 		            'label' : 'Populus tremuloides',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.424,
 		            'e0' : 52261.3
 		        }),
-		        new Species({   'id': "populus_grandidentata",
+		        new Species({   
+		        	'id': "populus_grandidentata",
 		            'label' : 'Populus grandidentata',
 		            't0' : 10,
 		            'k' :  283.15,
 		            'r0' : 0.294,
 		            'e0' : 59425.5
 		        }),
-		        new Species({   'id': "betula_lenta",
+		        new Species({   
+		        	'id': "betula_lenta",
 		            'label' : 'Betula lenta',
 		            't0' : 10,
 		            'k' :  283.15,
@@ -123,10 +160,9 @@ jQuery(document).ready(function() {
 		        })
 		    ]);
 		    this.tmp_species = {'species-name': '', 'species-base-temp': '', 'species-R0': '', 'species-E0': ''};
-		    //this.tmp_species = clone(predefinedSpecies[])
-		    this.species_list = new SpeciesCollection({ '1' : this.tmp_species});
 		    this.template = _.template(jQuery('#scenario-template').html());
 		    this.item_view = SpeciesRowView;
+		    this.species_container = this.$el.find(jQuery('.species-row-container'));
 		    console.log("Initialize ScenarioRowView");
 		},
 		
@@ -149,19 +185,14 @@ jQuery(document).ready(function() {
 	    
 	    selectSpecies: function(e)
 	    {
-	   		//console.log("e.currentTarget.attributes['id'].value");
-	   		//console.log(e.currentTarget.attributes['id'].value);
-	   		console.log(this.predefinedSpecies);
+	    	this.$el.find(jQuery('.species-predefined-list')).hide();
 	   		var get_species = this.predefinedSpecies.get([e.currentTarget.attributes['id'].value]);
-	   		//this.predefinedSpecies.get(e.currentTarget.attributes['id'].value);
-	   		console.log("this.predefinedSpecies");
-	   		console.log(this.predefinedSpecies);
-	   		console.log("get_species");
-	   		console.log(get_species);
-	   		//var temp_species = jQuery.extend(true, {}, this.predefinedSpecies[e.currentTarget.attributes.id]);
-	   		//var temp_species = clone(this.predefinedSpecies[e.currentTarget.attributes.id]);
-	   		//console.log(temp_species);
-	   	    //this.tmp_species = {'species-name': '', 'species-base-temp': '', 'species-R0': '', 'species-E0': ''};
+	   		var temp_species = get_species.clone();
+	   		this.speciesListView.collection.add(temp_species);
+	   		this.species_container.append(new SpeciesRowView(temp_species));
+	   		//this.speciesListView.render();
+	   		//this.speciesListView.render();
+	   		//this.tmp_species = {'species-name': '', 'species-base-temp': '', 'species-R0': '', 'species-E0': ''};
 	   		//this.species_list.create();
 	    	//var specHtml = this.$el.find(jQuery('.species-predefined-choice').attr('id'));
 	    	//var specHtml = this.attr('name');
@@ -182,15 +213,9 @@ jQuery(document).ready(function() {
 			_.bindAll(this, 'renderCollection', 'addItem');
 		    this.collection = new ScenarioCollection(options);
 	        this.collection.on('reset', this.renderCollection);
-	        //this.$el = $('#scenario-row-box');
 	        this.collection.on('add', this.addItem);
 	        this.item_view = ScenarioRowView;
-	        //this.temp_scenario = {'name' : 'NameHere'}
-	        //this.$el.append(jQuery('#scenario-template').html());
-	        this.collection.add();//(this.temp_scenario);
-	        //this.$el.find();
-	        
-	        console.log("Initialize ScenarioRowView");
+	        console.log("Initialize ScenarioCollectionView");
 		},
 		
 	   	events: {
@@ -206,20 +231,41 @@ jQuery(document).ready(function() {
 	});
 	
 	var CanopyScenarioGraphView = Backbone.View.extend({
-		/* Slowly Moving Old Code Over - Starting with Species.js will probably eliminate a lot of it */
 				
 	   	initialize: function (options) {
 	   		_.bindAll(this,
 	                  'addScenario');
 	   		this.options = options;
 	   		this.numScenarios = 0;
+	   		/* Attach the graph view to the pane its self so it has
+	   		 * access to all possible DOM children - is management view
+	   		 */
 	   		this.$el = $('#canopy-graph-pane');
 	   	    /* For now tying to the entire canopy tab-pane */
-	   	    //this.scenario_list_view = new ScenarioRowView();//);
-	   		//this.scenario_container = $('#scenario-row-box');
 	   		this.scenario_list_view = new ScenarioCollectionView({el: jQuery('#scenario-row-box')});
-	   		this.temp_scenario = {'name' : 'NameHere'}
-	   		this.scenario_list_view.collection.add(this.temp_scenario);
+	   		var temp_thing = new ScenarioRowView(new Scenario({'scenario-name': 'Initial Scenario'}));
+	   		console.log("temp_thing");
+	   		console.log(temp_thing);
+	   		//this.scenario_list_view.append(temp_thing);
+	   		//this.scenario_list_view.add();
+	   		//this.temp_scenario = new Scenario();
+	   		//{ 
+	   		//	'scenario-name': "Initial Scenario"
+	   		//}
+	   		console.log("this.scenario_list_view.collection");
+	   		console.log(this.scenario_list_view.collection);
+	   		this.scenario_list_view.collection.add(new Scenario({'scenario-name': 'Initial Scenario'}));
+	   		//console.log("this.temp_scenario");
+	   		//console.log(this.temp_scenario);
+//	   		this.temp_scenario = 	 new Species({   
+//	   			'id': "quercus_rubra",
+//	   			'label' : 'Quercus rubra',
+//	            't0' : 10,
+//	            'k' :  283.15,
+//	            'r0' : 0.602,
+//	            'e0' : 43140
+//	   		}
+	   		//this.scenario_list_view.collection.add(this.temp_scenario);
 	   	    //console.log("Initialize CanopyScenarioGraphView");
 	   	    //console.log("this.$el");
 	   	    //console.log(this.$el);
@@ -335,72 +381,72 @@ jQuery(document).ready(function() {
     });
 //});
   
-    $('#container2').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Scenario Graph'
-        },
-        subtitle: {
-            text: ''
-        },
-        xAxis: {
-            categories: [
-                'Jan',
-                'Feb',
-                'Mar',
-                'Apr',
-                'May',
-                'Jun',
-                'Jul',
-                'Aug',
-                'Sep',
-                'Oct',
-                'Nov',
-                'Dec'
-            ],
-            crosshair: true
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Seasonal Respiratory Carbon Release mol Cm-2'
-            }
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'Tokyo',
-            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-        }, {
-            name: 'New York',
-            data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-        }, {
-            name: 'London',
-            data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-        }, {
-            name: 'Berlin',
-            data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-        }]
-    });
-    
+//    $('#container2').highcharts({
+//        chart: {
+//            type: 'column'
+//        },
+//        title: {
+//            text: 'Scenario Graph'
+//        },
+//        subtitle: {
+//            text: ''
+//        },
+//        xAxis: {
+//            categories: [
+//                'Jan',
+//                'Feb',
+//                'Mar',
+//                'Apr',
+//                'May',
+//                'Jun',
+//                'Jul',
+//                'Aug',
+//                'Sep',
+//                'Oct',
+//                'Nov',
+//                'Dec'
+//            ],
+//            crosshair: true
+//        },
+//        yAxis: {
+//            min: 0,
+//            title: {
+//                text: 'Seasonal Respiratory Carbon Release mol Cm-2'
+//            }
+//        },
+//        tooltip: {
+//            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+//            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+//                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+//            footerFormat: '</table>',
+//            shared: true,
+//            useHTML: true
+//        },
+//        plotOptions: {
+//            column: {
+//                pointPadding: 0.2,
+//                borderWidth: 0
+//            }
+//        },
+//        series: [{
+//            name: 'Tokyo',
+//            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+//
+//        }, {
+//            name: 'New York',
+//            data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+//
+//        }, {
+//            name: 'London',
+//            data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+//
+//        }, {
+//            name: 'Berlin',
+//            data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+//
+//        }]
+//    });
+//    
     
     
     
